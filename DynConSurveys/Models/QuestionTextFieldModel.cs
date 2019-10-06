f0306cbd9d061410250b7906386d0333e47a55a4
David V. Corbin <david.corbin@live.com>
2019-10-06 09:46:27 -0400

    Updates for PolymorphicPostback

---
+++ DynConSurveys/Models/QuestionTextFieldModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynConSurveys.Models
+{
+    public class QuestionTextFieldModel : QuestionBaseModel
+    {
+        public string Answer { get; set; }
+
+        public override string QuestionType => "QuestionTextFieldModel";
+    }
+}
